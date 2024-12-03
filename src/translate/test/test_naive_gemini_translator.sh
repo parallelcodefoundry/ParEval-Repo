f33cfe1acbbbeb23eecbce76863b138c6ec5e8ad
Josh Davis <jhdavis@umd.edu>
2024-12-03 13:42:14 -0500

    Naive gemini translator script updates

--- src/translate/test/test_naive_gemini_translator.sh
+++ src/translate/test/test_naive_gemini_translator.sh
@@ -1,4 +1,4 @@
 for iter in {1..5}; do
-    python3 ../translate.py --input ../../../targets/SimpleMOC-kernel/cuda --output ../../../data/SimpleMOC-kernel/naive-gemini-cuda-to-omp --output-id ${iter} --app-name SimpleMOC-kernel --method naive --naive-llm gemini --src-model cuda --dst-model openmp-offload --force-overwrite --log-interactions
+    python3 ../translate.py --input ../../../targets/XSBench/cuda --output ../../../data/XSBench/naive-gemini-cuda-to-omp --output-id ${iter} --app-name xsbench --method naive --naive-llm gemini --src-model cuda --dst-model openmp-offload --force-overwrite --log-interactions
     sleep 15
 done
