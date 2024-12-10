d305780f6b697b1ec950190c6837d6c899bbbdf3
Josh Davis <jhdavis@umd.edu>
2024-12-10 13:54:44 -0800

    Add ollama wrapper script, improve naive runner

--- src/translate/test/naive_translate.sh
+++ src/translate/test/naive_translate.sh
@@ -12,10 +12,16 @@ elif [ "$#" -eq 2 ]; then
     application=$2
     echo "Running all experiments for ${llm_version} and ${application}"
     for iter in {1..5}; do
-        python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f
+        python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f --hide-progress
         sleep 15
     done
+elif [ "$#" -eq 3 ]; then
+    llm_version=$1
+    application=$2
+    iter=$3
+    echo "Running one experiment for ${llm_version} and ${application}, output ID ${iter}"
+    python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f --hide-progress
 else
-    echo "Usage: naive_translate.sh [llm_version] [application]"
+    echo "Usage: naive_translate.sh [llm_version] [application] [outputid]"
     exit 1
 fi
