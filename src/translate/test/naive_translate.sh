17255090a7e3dcbab74872eef0373e48ebaefc10
Josh Davis <jhdavis@umd.edu>
2024-12-11 11:57:00 -0800

    Don't hide progress in naive translate

--- src/translate/test/naive_translate.sh
+++ src/translate/test/naive_translate.sh
@@ -12,7 +12,7 @@ elif [ "$#" -eq 2 ]; then
     application=$2
     echo "Running all experiments for ${llm_version} and ${application}"
     for iter in {1..5}; do
-        python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f --hide-progress
+        python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f
         sleep 15
     done
 elif [ "$#" -eq 3 ]; then
@@ -20,7 +20,7 @@ elif [ "$#" -eq 3 ]; then
     application=$2
     iter=$3
     echo "Running one experiment for ${llm_version} and ${application}, output ID ${iter}"
-    python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f --hide-progress
+    python3 ../translate.py --input ../../../targets/${application}/cuda --config ../../../targets/${application}/openmp-offload --output ../../../data/${application}/naive-${llm_version}-cuda-to-omp --output-id ${iter} --app-name ${application} --method naive --naive-llm ${llm_version} --src-model cuda --dst-model openmp-offload --log-interactions -f
 else
     echo "Usage: naive_translate.sh [llm_version] [application] [outputid]"
     exit 1
