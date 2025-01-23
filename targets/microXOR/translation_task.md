1a7e482f1c4fb3cf262fca7940b4cce832fc39ab
Ishan Khillan <ishankhillan@gmail.com>
2025-01-22 17:13:42 -0800

    Added first run of SWE-agent on microXOR

--- targets/microXOR/translation_task.md
+++ targets/microXOR/translation_task.md
@@ -6,6 +6,6 @@ You are helping a software developer translate a codebase from the CUDA executio
 
 The codebase is called microXOR, a stencil computation benchmark computing an XOR operation over a 2D grid of cells.
 
-Given this code repository, translate microXOR (located in targets/microXOR/cuda) codebase's CUDA files to the OpenMP Offload execution model.
+Given this code repository, translate microXOR codebase's CUDA files to the OpenMP Offload execution model.
 
-The new files should be in C++ (.cpp or .hpp files) and the Makefile should be updated to compile with g++.
+The new files should be in C++ (.cpp or .hpp files), and all old CUDA files must be deleted. A new Makefile should be made to compile with g++.
--- test/test_swe-agent_translator.sh
+++ test/test_swe-agent_translator.sh
@@ -1,15 +1,9 @@
-for iter in {0..0}; do
-    python3 /global/homes/i/ikhillan/code-translation/src/agent/swe-agent-translator.py \
-        --repo_path "/global/homes/i/ikhillan/tmp/microXOR_cuda_repo" \
-        --output_base_dir "/global/homes/i/ikhillan/code-translation/data/microXOR/SWE-agent-cuda-to-omp" \
-        --keys_cfg "/global/homes/i/ikhillan/SWE-agent/keys.cfg" \
-        --config_file "/global/homes/i/ikhillan/SWE-agent/config/default.yaml" \
-        --data_path "/global/homes/i/ikhillan/code-translation/targets/microXOR/translation_task.md" \
-        --model_name "vllm:llama3.3" \
-        --host_url "http://localhost:8000" \
-        --input_directory "/global/homes/i/ikhillan/code-translation/targets/microXOR/cuda"
-
-    python3 /global/homes/i/ikhillan/code-translation/src/agent/util/clean_ansi.py \
-        --input_file "/global/homes/i/ikhillan/code-translation/data/microXOR/SWE-agent-cuda-to-omp/output-0/output-0-swe-agent-comments.txt" \
-        --output_file "/global/homes/i/ikhillan/code-translation/data/microXOR/SWE-agent-cuda-to-omp/output-0/cleaned-comments.txt"
+for iter in {0..4}; do
+    python3 /Users/ishan/pssg/code-translation/src/SWE-agent/swe-agent-translator.py \
+        --agent_model_name gpt-4o-mini \
+        --per_instance_cost_limit 0.06 \
+        --repo_path /Users/ishan/pssg/code-translation/targets/microXOR/cuda \
+        --deployment_image python \
+        --problem_statement_path /Users/ishan/pssg/code-translation/targets/microXOR/translation_task.md \
+        --output_dir /Users/ishan/pssg/code-translation/data/microXOR/SWE-agent-cuda-to-omp/output-$iter
 done
