0ec15e9c54d59074a2d7a4db82a5bcbf71eb842e
Ishan Khillan <ishankhillan@gmail.com>
2025-02-14 15:24:00 -0500

    Testing using target.json as problem statement

--- test/test_swe-agent_translator.sh
+++ test/test_swe-agent_translator.sh
@@ -1,9 +1,9 @@
-for iter in {1..4}; do
+for iter in {5..5}; do
    python3 /Users/ishan/pssg/code-translation/src/SWE-agent/swe-agent-translator.py \
        --agent_model_name gpt-4o-mini \
        --per_instance_cost_limit 0.06 \
        --repo_path /Users/ishan/pssg/code-translation/targets/SimpleMOC-kernel/cuda \
        --deployment_image python \
-       --problem_statement_path /Users/ishan/pssg/code-translation/targets/SimpleMOC-kernel/cuda/translation_task.md \
-       --output_dir /Users/ishan/pssg/code-translation/data/SimpleMOC-kernel/SWE-agent-cuda-to-omp/output-$iter
+       --problem_statement_path /Users/ishan/pssg/code-translation/targets/SimpleMOC-kernel/cuda/target.json \
+       --output_dir /Users/ishan/pssg/code-translation-results/SimpleMOC-kernel/SWE-agent-cuda-to-omp/output-$iter
 done
