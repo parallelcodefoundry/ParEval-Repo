1d009284406afb463629926f36dd381a8f964692
Ishan Khillan <ishankhillan@gmail.com>
2025-02-28 15:24:22 -0500

    Implemented helper file to clean translated SWE-agent repos of any old .cu and .cuh files

--- test/test_swe-agent_translator.sh
+++ test/test_swe-agent_translator.sh
@@ -1,5 +1,5 @@
 for iter in {5..5}; do
-   python3 /Users/ishan/pssg/code-translation/src/SWE-agent/translation_task_generator.py \
+   python3 /Users/ishan/pssg/code-translation/src/SWE-agent/helper/translation_task_generator.py \
        --target_json /Users/ishan/pssg/code-translation/targets/SimpleMOC-kernel/cuda/target.json \
        --output_file /Users/ishan/pssg/code-translation/targets/SimpleMOC-kernel/cuda/translation_task.md
 
@@ -11,5 +11,8 @@ for iter in {5..5}; do
        --problem_statement_path /Users/ishan/pssg/code-translation/targets/SimpleMOC-kernel/cuda/translation_task.md \
        --output_dir /Users/ishan/pssg/code-translation-results/SimpleMOC-kernel/SWE-agent-cuda-to-omp/output-$iter \
        --output_id $iter
+
+    python3 /Users/ishan/pssg/code-translation/src/SWE-agent/helper/clean_output_repo.py \
+        --translated_repo /Users/ishan/pssg/code-translation-results/SimpleMOC-kernel/SWE-agent-cuda-to-omp/output-$iter
 
 done
