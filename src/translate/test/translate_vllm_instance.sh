fdee76cc0136c1d2a3e2782e98ef1c61737e1606
Joshua Davis <jhdavis@umd.edu>
2025-04-04 11:28:46 -0500

    Progress on delta translate scripts

--- src/translate/test/translate_vllm_instance.sh
+++ src/translate/test/translate_vllm_instance.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 get_usage () {
-    echo "Usage: translate_vllm_instance.sh method llm_name application firstid lastid outpath"
+    echo "Usage: translate_vllm_instance.sh method llm_name application firstid lastid outpath hostname"
     echo "method: the method to use (e.g., naive, restate)"
     echo "llm_name: the name of the LLM to use (e.g., Llama-3.3-70b, QwQ-32b)"
     echo "application: the name of the application to translate"
@@ -8,6 +9,7 @@ get_usage () {
     echo "fisrtid: integer ID for the first iteration to execute"
     echo "lastid: integer ID for the last iteration to execute"
     echo "outpath: file path to write all output for the run"
+    echo "hostname: name of the system being used, determines configuration paths"
 }
 
 run_vllm_translation () {
@@ -18,32 +20,23 @@ run_vllm_translation () {
     LITER=$5
     llm_path=$LLM_NAME
 
-    # If model is already downloaded, then copy to /dev/shm/
+    # If model is already downloaded, then copy to $LOCAL_MEM/
     if [[ $LLM_NAME == *"gguf"* ]]; then
-        llm_path=/dev/shm/$LLM_NAME
-        echo -n "Copying model to /dev/shm/... "
-        cp $SCRATCH/$LLM_NAME /dev/shm/
+        llm_path=$LOCAL_MEM/$LLM_NAME
+        echo -n "Copying model to $LOCAL_MEM/... "
+        cp $SCRATCH/$LLM_NAME $LOCAL_MEM/
         echo "Done"
     fi
 
     # Activate vLLM environment
-    source /home/jhdavis/venvs/2024-04-29-vllm/bin/activate
-
-    # Set environment variables
-    export HTTP_PROXY="http://proxy.alcf.anl.gov:3128"
-    export HTTPS_PROXY="http://proxy.alcf.anl.gov:3128"
-    export LD_LIBRARY_PATH=$HOME/venvs/2024-04-29-vllm/lib64/python3.11/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
-    export TMPDIR=/tmp/vllm && mkdir -p $TMPDIR
-    export VLLM_HOST_IP=127.0.0.1
-    export NO_PROXY=127.0.0.1,localhost
-    export CUDA_VISIBLE_DEVICES=0,1,2,3
+    source $VLLM_VENV
 
     # Start vLLM server
     echo -n "Launching vLLM server process... "
     unset vllm_flags
     if [[ $LLM_NAME == *"Llama-3.3-70B"* ]]; then
         vllm_flags="--tokenizer meta-llama/Llama-3.3-70B-Instruct"
-    elif [[ $LLM_NAME == *"QwQ"* ]]; then
+    elif [[ $LLM_NAME == *"QwQ"* || $LLM_NAME == *"qwq"* ]]; then
         vllm_flags="--enable-reasoning --reasoning-parser deepseek_r1"
     fi
     vllm serve $llm_path \
@@ -71,9 +64,7 @@ run_vllm_translation () {
 
     # Do translation
     echo "Running translation $METHOD $llm_path vllm $APPLICATION $FITER $LITER... "
-    cd /home/jhdavis/llms4hpc/code-translation/src/translate/test
-    module use /soft/modulefiles
-    module load llvm cudatoolkit-standalone
+    cd $SCRIPT_DIR
     bash translate.sh $METHOD $llm_path vllm $APPLICATION $FITER $LITER
     echo "Done"
 }
@@ -82,7 +73,30 @@ stamp () {
     echo "[$$ | $(date '+%Y-%m-%d %H:%M:%S')]"
 }
 
-if [ "$#" -eq 6 ]; then
+if [ "$#" -eq 7 ]; then
+    SYSTEM=$7
+    if [[ $SYSTEM == "delta" ]]; then
+        LOCAL_MEM=/tmp
+        VLLM_VENV=/u/jhdavis/vllm_venv/bin/activate
+        SCRIPT_DIR=/u/jhdavis/code-translation/src/translate/test
+        export TMPDIR=/tmp/vllm && mkdir -p $TMPDIR
+        export CUDA_VISIBLE_DEVICES=0,1,2,3
+        module load llvm cuda/12.3.0
+    elif [[ $SYSTEM == "polaris" ]]; then
+        LOCAL_MEM=/dev/shm
+        VLLM_VENV=/home/jhdavis/venvs/2024-04-29-vllm/bin/activate
+        SCRIPT_DIR=/home/jhdavis/llms4hpc/code-translation/src/translate/test
+        export HTTP_PROXY="http://proxy.alcf.anl.gov:3128"
+        export HTTPS_PROXY="http://proxy.alcf.anl.gov:3128"
+        export LD_LIBRARY_PATH=$HOME/venvs/2024-04-29-vllm/lib64/python3.11/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
+        export TMPDIR=/tmp/vllm && mkdir -p $TMPDIR
+        export VLLM_HOST_IP=127.0.0.1
+        export NO_PROXY=127.0.0.1,localhost
+        export CUDA_VISIBLE_DEVICES=0,1,2,3
+        module use /soft/modulefiles
+        module load llvm cudatoolkit-standalone
+    fi
+
     echo "$(stamp) Starting vLLM translation instance, writing to $6"
     run_vllm_translation "$1" "$2" "$3" "$4" "$5" &> $6
     echo "$(stamp) Finished vLLM translation instance, written to $6"
